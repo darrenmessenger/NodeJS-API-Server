120ef1d85475dd58fbe6d62134870b0a4e875541
Darren Messenger <39158691+darrenmessenger@users.noreply.github.com>
2023-03-03 09:40:16 +0000

    Getting all events

--- index.js
+++ index.js
@@ -9,12 +9,32 @@ app.get("/getAllSports",(req, res) =>{
     .then((response) => {
         // retrieve all sports and return.
         const newData = response.data.result.sports;
+        if(newData.length == 0) responseArray = "No Data Found";
         res.send(newData);
     }).catch((err) => {
         res.send(err);
     });
 })
 
+// API to get all of the events
+app.get("/getAllEvents",(req, res) =>{
+    axios.get("https://partners.betvictor.mobi/en-gb/in-play/1/events")
+    .then((response) => {
+        let responseArray = [];
+        // retrieve all sports.
+        const sportsData = response.data.result.sports;
+        // loop through data to retrieve all of the events
+        for (const i in sportsData) {
+            responseArray.push(sportsData[i].comp);
+        }
+        if(responseArray.length == 0) responseArray = "No Data Found";
+        res.send(responseArray);
+    }).catch((err) => {
+        res.send(err);
+    });
+})
+
+// connect the server
 app.listen(PORT,() => {
     console.log(`Running Server on Port ${PORT}`);
 })
