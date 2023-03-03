024323c4486493268c9e1dbf186a3b48429a714c
Darren Messenger <39158691+darrenmessenger@users.noreply.github.com>
2023-03-03 09:23:32 +0000

    Use Axios to retrieve all data

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require("express"); // require the express framework
 const PORT = 8080;
 const app = express();
 
-app.get("/",(req, res) =>{
-        res.send("Hello");
-  
+// API to get all of the sports
+app.get("/getAllSports",(req, res) =>{
+    axios.get("https://partners.betvictor.mobi/en-gb/in-play/1/events")
+    .then((response) => {
+        // retrieve all sports and return.
+        const newData = response.data.result.sports;
+        res.send(newData);
+    }).catch((err) => {
+        res.send(err);
+    });
 })
 
 app.listen(PORT,() => {
